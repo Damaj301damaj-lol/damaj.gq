57c53436874667a6808e955f7ba0ccebd23baa88
Mohamad Damaj <mohamad301damaj@duck.com>
2023-02-19 00:31:52 +0200

    More Modifications

--- templates/footer.php
+++ templates/footer.php
@@ -1,3 +1,3 @@
 <hr>
-<p>site written and hosted by <a href="https://free-werewolf.top/">Emily</a>, Modified by me on vim. Massive thanks to her, she is awesome!</p>
+<p>site written and icons and hosted by <a href="https://free-werewolf.top/">Emily</a>, Modified by me on vim and VSCode. Massive thanks to her, she is awesome!</p>
 <div class="code">This site is <a href="https://github.com/Damaj301damaj-lol/damaj.gq">open source!</a></div>
