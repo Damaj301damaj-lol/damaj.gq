4c15de2c37d97d03347e5a4245eee09a6fd5a585
Mohamad Damaj <mohamad301damaj@duck.com>
2023-02-19 00:30:18 +0200

    Fix Machine related issues

--- site/contact/index.php
+++ site/contact/index.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<title>damaj.gq</title>
-		<link rel="stylesheet" href="../css/main.css">
+		<link rel="stylesheet" href="/css/main.css">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
                 <meta content="damaj.tech" property="og:title" />
                 <meta content="Contact Me!" property="og:description" />
@@ -11,10 +11,10 @@
                 <meta content="#FD7717" data-react-helmet="true" name="theme-color" />
 	</head>
 	<body>
-<?php include '/templates/header.php';  ?>
+<?php include '/var/www/html/~mohamad/templates/header.php';  ?>
 
 		<p>If you ever need to contact me to chat or fix an issue, you can email me <a href="mailto:mohamad301damaj@duck.com">here</a> , or you can add me on Discord "Damaj301damaj#7279", It would be nice to talk :).</p>
 		<p>You may find me elsewhere too!</p>
-<?php include '/templates/footer.php' ; ?>
+<?php include '/var/www/html/~mohamad/templates/footer.php' ; ?>
 	</body>
 </html>
