f8d1f8c2aa4fa9b3f09f8945cdf38b7d1239ec77
Mohamad Damaj <mohamad301damaj@duck.com>
2023-03-09 10:17:16 +0200

    Updated site: remove .gitignore, add old/index.php

---
+++ old/index.php
@@ -0,0 +1,7 @@
+<head>
+    <title>EOL</title>
+    <meta http-equiv="refresh" content="0; url=https://damaj.tech" />
+</head>
+<body>
+    <p>Redirecting to <a href="https://damaj.tech">https://damaj.tech</a></p>
+</body>
