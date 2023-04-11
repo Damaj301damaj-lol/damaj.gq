7a4de024ad8706ffed3b4bbb9428886a8433b4d2
Mohamad Damaj <mohamad301Damaj@duck.com>
2023-04-11 23:02:41 +0300

    Update footer.php

--- templates/footer.php
+++ templates/footer.php
@@ -1,3 +1,3 @@
 <hr>
 <middle><p>Hosted entirely by myself, you may check the <a href="/site/history">site history</a> to learn how this site switched hands.</p>
-<div class="code">This site is <a href="https://github.com/Damaj301damaj-lol/damaj.tech">open source!</a></div></middle>
+<div class="code">This site is <a href="https://github.com/Damaj301damaj-lol/damaj.tech">Libre!</a></div></middle>
