acc317a939b9d82a914dfcd33d8da351f950edef
Mohamad Damaj <mohamad301damaj@duck.com>
2023-05-05 23:01:12 +0300

    More fixing

--- site/others/index.php
+++ site/others/index.php
@@ -12,11 +12,11 @@
 		
 	</head>
 	<body>
-<?php include '/templates/header.php';  ?>
+<?php include '/var/www/html/templates/header.php';  ?>
 
 		<h1>Checkout my other websites :D<h1>
 		<p> <a href=https://free-wii-u.gq>free-wii-u.gq</a>
 		<p> <a href=https://soystemd.gq>soystemd.gq</a> </p>
-<?php include '/templates/footer.php' ; ?>
+<?php include '/var/www/html/templates/footer.php' ; ?>
 	</body>
 </html>
